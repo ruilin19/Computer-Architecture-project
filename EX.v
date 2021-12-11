47f5aec6226e0d1d175692477db8315fd628f50c
wang will <56262804+wangwill0713@users.noreply.github.com>
2021-12-11 22:54:39 +0800

    Add files via upload

--- EX.v
+++ EX.v
@@ -20,11 +20,15 @@ module EX(
     output wire [5:0]ex_aluop_o,
     output wire [4:0]ex_addr_o
 );
-    
+    reg ex_stop;
     reg [`ID_TO_EX_WD-1:0] id_to_ex_bus_r;
+    wire [`ID_TO_EX_WD-1:0] id_to_ex_bus_rr;
     wire ex_to_iden;//ex传给下一条流水的ID端的使能信号
     wire [4:0] ex_to_iregaddr;//ex传给下一条流水的ID端的数据的地址
     wire [31:0] ex_to_idata;//ex传给下一条流水的ID端的数据
+    wire ex_id_hlwe; //ex to id hl_we
+    wire [1:0]ex_id_hlwaddr;//ex to id hl_waddr
+    wire [63:0] ex_id_hlwdata;//ex to id hl_wdata
     always @ (posedge clk) begin
         if (rst) begin
             id_to_ex_bus_r <= `ID_TO_EX_WD'b0;
@@ -34,11 +38,17 @@ module EX(
         // end
         else if (stall[2]==`Stop && stall[3]==`NoStop) begin
             id_to_ex_bus_r <= `ID_TO_EX_WD'b0;
+            ex_stop=1'b1;
         end
         else if (stall[2]==`NoStop) begin
             id_to_ex_bus_r <= id_to_ex_bus;
+            ex_stop=1'b0;
+        end
+        else if(stall[2]==`Stop && stall[3]==`Stop) begin
+            ex_stop=1'b1;
         end
     end
+    assign id_to_ex_bus_rr=id_to_ex_bus_r;
 
     wire [31:0] ex_pc, inst;//指令地址以及指令
     wire [11:0] alu_op;//运算类型
@@ -48,8 +58,11 @@ module EX(
     wire [3:0] data_ram_wen;//访存信号
     wire rf_we;//读寄存器使能信号
     wire [4:0] rf_waddr;//寄存器的位置
+    wire hl_we;//hl we
+    wire [1:0]hl_waddr;// hl address
     wire sel_rf_res;//选择的结果
     wire [31:0] rf_rdata1, rf_rdata2;//从寄存器中读入的数据
+    wire [63:0] hl_rdata;//hl data
 //    wire [31:0] ex_save_inst;//要保存的指令
 //    wire [31:0] pc_plus_8;
 //    assign is_next_indelayslot_id_o=is_next_indelayslot_id_i;
@@ -57,6 +70,9 @@ module EX(
     assign ex_aluop_o=inst[31:26];
     assign ex_addr_o =rf_waddr;
     assign {
+        hl_rdata,       //225:162
+        hl_waddr,       //161:160
+        hl_we,          //159
         ex_pc,          // 158:127
         inst,           // 126:95
         alu_op,         // 94:83
@@ -67,24 +83,30 @@ module EX(
         rf_we,          // 70
         rf_waddr,       // 69:65
         sel_rf_res,     // 64
-        rf_rdata1,         // 63:32
-        rf_rdata2          // 31:0
-    } = id_to_ex_bus_r;
+        rf_rdata1,      // 63:32
+        rf_rdata2       // 31:0
+    } = ex_stop?id_to_ex_bus_rr:id_to_ex_bus_r;
 
     wire [31:0] imm_sign_extend, imm_zero_extend, sa_zero_extend;
     assign imm_sign_extend = {{16{inst[15]}},inst[15:0]};//对立即数做有符号扩展
     assign imm_zero_extend = {16'b0, inst[15:0]};//对立即数做无符号扩展
     assign sa_zero_extend = {27'b0,inst[10:6]};//对sa做无符号扩展
-
+    //判断是不是mfhi或者mflo
+    wire mf_hi;
+    wire mf_lo;
+    assign mf_hi = (inst[31:16] == 16'b0)&&(inst[10:6] == 5'b0)&&(inst[5:0] == 6'b01_0000);
+    assign mf_lo = (inst[31:16] == 16'b0)&&(inst[10:6] == 5'b0)&&(inst[5:0] == 6'b01_0010);
+    
     wire [31:0] alu_src1, alu_src2;
     wire [31:0] alu_result, ex_result;
     
-    assign alu_src1 = sel_alu_src1[1] ? ex_pc :
+    assign alu_src1 =sel_alu_src1[1] ? ex_pc :
                       sel_alu_src1[2] ? sa_zero_extend : rf_rdata1;
-
-    assign alu_src2 = sel_alu_src2[1] ? imm_sign_extend :
+    
+    assign alu_src2 =sel_alu_src2[0] ? rf_rdata2:
+                      sel_alu_src2[1] ? imm_sign_extend :
                       sel_alu_src2[2] ? 32'd8 :
-                      sel_alu_src2[3] ? imm_zero_extend : rf_rdata2;
+                      sel_alu_src2[3] ? imm_zero_extend :32'b0 ;
     //跟据sel的类型选择合适的操作数来源
     alu u_alu(
     	.alu_control (alu_op ),
@@ -94,16 +116,35 @@ module EX(
     );
 //    assign ex_save_inst=(alu_op==12'd0)?pc_plus_8:32'd0;
 //    assign ex_result =rf_we&(rf_waddr==32'd31)?ex_save_inst: alu_result;//ALU计算结果
-    assign ex_result = alu_result;
+//    assign ex_result = hl_we?hl_rdata:alu_result;
+    assign ex_result = mf_lo ? hl_rdata[63:32]:
+                        mf_hi ? hl_rdata[31:0]:alu_result;
     assign data_sram_en=data_ram_en;
-    assign data_sram_wen=data_ram_wen;
+    assign data_sram_wen=(data_ram_wen==4'b0001)&&(data_sram_addr[1:0]==2'b00)?4'b0001:
+                          (data_ram_wen==4'b0001)&&(data_sram_addr[1:0]==2'b01)?4'b0010:
+                          (data_ram_wen==4'b0001)&&(data_sram_addr[1:0]==2'b10)?4'b0100:
+                          (data_ram_wen==4'b0001)&&(data_sram_addr[1:0]==2'b11)?4'b1000:
+                          (data_ram_wen==4'b0011)&&(data_sram_addr[1:0]==2'b00)?4'b0011:
+                          (data_ram_wen==4'b0011)&&(data_sram_addr[1:0]==2'b10)?4'b1100:
+                          (data_ram_wen==4'b1111)?4'b1111:4'b0000;
     assign data_sram_addr=data_ram_en?ex_result:32'd0;
-    assign data_sram_wdata=(data_ram_wen==4'b0001)?rf_rdata2:
-                           (data_ram_wen==4'b0011)?rf_rdata2:
-                           (data_ram_wen==4'b1111)?rf_rdata2:32'd0;
+    assign data_sram_wdata=(data_ram_wen==4'b0001)&&(data_sram_addr[1:0]==2'b00)?{24'b0,rf_rdata2[7:0]}:
+                            (data_ram_wen==4'b0001)&&(data_sram_addr[1:0]==2'b01)?{16'b0,rf_rdata2[7:0],8'b0}:
+                            (data_ram_wen==4'b0001)&&(data_sram_addr[1:0]==2'b10)?{8'b0,rf_rdata2[7:0],16'b0}:
+                            (data_ram_wen==4'b0001)&&(data_sram_addr[1:0]==2'b11)?{rf_rdata2[7:0],24'b0}:
+                            (data_ram_wen==4'b0011)&&(data_sram_addr[1:0]==2'b00)?{16'b0,rf_rdata2[15:0]}:
+                            (data_ram_wen==4'b0011)&&(data_sram_addr[1:0]==2'b10)?{rf_rdata2[15:0],16'b0}:
+                            (data_ram_wen==4'b1111)?rf_rdata2:32'd0;
     wire [5:0] ld_type_o;
+    wire [1:0] addr_ram_o;
     assign ld_type_o=inst[31:26];
+    assign addr_ram_o=data_sram_addr[1:0];
+    wire [63:0] muldiv_mt_hl;
     assign ex_to_mem_bus = {
+        addr_ram_o,           //150:149
+        muldiv_mt_hl,       //148:85
+        hl_waddr,       //84:83
+        hl_we,          //82
         ld_type_o,      //81:76
         ex_pc,          // 75:44
         data_ram_en,    // 43
@@ -113,12 +154,132 @@ module EX(
         rf_waddr,       // 36:32
         ex_result       // 31:0
     };
-    assign ex_to_iden=rf_we;
-    assign ex_to_iregaddr=rf_waddr;
-    assign ex_to_idata=ex_result;
+    //乘法部分
+    wire [63:0] mul_result;
+    wire inst_mult;
+    wire inst_multu;
+    wire mul_signed; // 有符号乘法标记
+    assign inst_mult = (inst[31:26]==6'b0)&(inst[15:6]==10'b0)&(inst[5:0]==6'b01_1000);
+    assign inst_multu = (inst[31:26]==6'b0)&(inst[15:6]==10'b0)&(inst[5:0]==6'b01_1001);
+    assign mul_signed = inst_mult;
+    
+    mul u_mul(
+    	.clk        (clk            ),
+        .resetn     (~rst           ),
+        .mul_signed (mul_signed     ),
+        .ina        (alu_src1       ), // 乘法源操作数1
+        .inb        (alu_src2       ), // 乘法源操作数2
+        .result     (mul_result     ) // 乘法结果 64bit
+    );
+    
+    
+    //除法部分
+    wire [63:0] div_result;
+    wire inst_div,inst_divu;
+    wire div_ready_i;
+    reg stallreq_for_div;
+    reg [31:0] div_opdata1_o;
+    reg [31:0] div_opdata2_o;
+    reg div_start_o;
+    reg signed_div_o;
+    assign stall_ex = stallreq_for_div;
+    assign inst_div=(inst[31:26]==6'b0)&(inst[15:6]==10'b0)&(inst[5:0]==6'b01_1010);
+    assign inst_divu =(inst[31:26]==6'b0)&(inst[15:6]==10'b0)&(inst[5:0]==6'b01_1011);
+    div u_div(
+    	.rst          (rst              ),
+        .clk          (clk              ),
+        .signed_div_i (signed_div_o     ),
+        .opdata1_i    (div_opdata1_o    ),
+        .opdata2_i    (div_opdata2_o    ),
+        .start_i      (div_start_o      ),
+        .annul_i      (1'b0             ),
+        .result_o     (div_result       ), // 除法结果 64bit
+        .ready_o      (div_ready_i      )
+    );
+
+    always @ (*) begin
+        if (rst) begin
+            stallreq_for_div = `NoStop;
+            div_opdata1_o = `ZeroWord;
+            div_opdata2_o = `ZeroWord;
+            div_start_o = `DivStop;
+            signed_div_o = 1'b0;
+        end
+        else begin
+            stallreq_for_div = `NoStop;
+            div_opdata1_o = `ZeroWord;
+            div_opdata2_o = `ZeroWord;
+            div_start_o = `DivStop;
+            signed_div_o = 1'b0;
+            case ({inst_div,inst_divu})
+                2'b10:begin
+                    if (div_ready_i == `DivResultNotReady) begin
+                        div_opdata1_o = rf_rdata1;
+                        div_opdata2_o = rf_rdata2;
+                        div_start_o = `DivStart;
+                        signed_div_o = 1'b1;
+                        stallreq_for_div = `Stop;
+                    end
+                    else if (div_ready_i == `DivResultReady) begin
+                        div_opdata1_o = rf_rdata1;
+                        div_opdata2_o = rf_rdata2;
+                        div_start_o = `DivStop;
+                        signed_div_o = 1'b1;
+                        stallreq_for_div = `NoStop;
+                    end
+                    else begin
+                        div_opdata1_o = `ZeroWord;
+                        div_opdata2_o = `ZeroWord;
+                        div_start_o = `DivStop;
+                        signed_div_o = 1'b0;
+                        stallreq_for_div = `NoStop;
+                    end
+                end
+                2'b01:begin
+                    if (div_ready_i == `DivResultNotReady) begin
+                        div_opdata1_o = rf_rdata1;
+                        div_opdata2_o = rf_rdata2;
+                        div_start_o = `DivStart;
+                        signed_div_o = 1'b0;
+                        stallreq_for_div = `Stop;
+                    end
+                    else if (div_ready_i == `DivResultReady) begin
+                        div_opdata1_o = rf_rdata1;
+                        div_opdata2_o = rf_rdata2;
+                        div_start_o = `DivStop;
+                        signed_div_o = 1'b0;
+                        stallreq_for_div = `NoStop;
+                    end
+                    else begin
+                        div_opdata1_o = `ZeroWord;
+                        div_opdata2_o = `ZeroWord;
+                        div_start_o = `DivStop;
+                        signed_div_o = 1'b0;
+                        stallreq_for_div = `NoStop;
+                    end
+                end
+                default:begin
+                end
+            endcase
+        end
+    end
+    
+    assign muldiv_mt_hl=(inst_mult|inst_multu)?{mul_result[31:0],mul_result[63:32]}:
+                         (inst_div|inst_divu)?{div_result[31:0],div_result[63:32]}:
+                         (hl_we&hl_waddr[1]==1'b1)?{ex_result,32'b0}:
+                         (hl_we&hl_waddr[1]==1'b0)?{32'b0,ex_result}:hl_rdata;
+    assign ex_to_iden = rf_we;
+    assign ex_to_iregaddr = rf_waddr;
+    assign ex_to_idata = ex_result;
+    assign ex_id_hlwe = hl_we;
+    assign ex_id_hlwaddr = hl_waddr;
+    assign ex_id_hlwdata = muldiv_mt_hl;
     assign ex_to_id_bus={
+        ex_id_hlwdata, //104:41
+        ex_id_hlwaddr,//40:39
+        ex_id_hlwe,//38
         ex_to_iden,//37
         ex_to_iregaddr,//36:32
         ex_to_idata//31:0
-    };
+};
 endmodule
--- ID.v
+++ ID.v
@@ -29,16 +29,25 @@ module ID(
     wire wb_rf_we;//WB段回写的信号
     wire [4:0] wb_rf_waddr;//WB段回写的数据地址
     wire [31:0] wb_rf_wdata;//WB段回写的数据
+    wire wb_hl_we;//wb hilo we
+    wire [1:0]wb_hl_waddr;//wb hilo address
+    wire [63:0] wb_hl_wdata;//wb hilo we data
+    
     wire ex_rf_we;//ex段回写使能信号
     wire [4:0] ex_rf_waddr;//ex段回写寄存器
     wire [31:0] ex_rf_wdata;//ex段回写数据
+    wire ex_hl_we;//ex hilo we
+    wire [1:0]ex_hl_waddr;//ex hilo address
+    wire [63:0] ex_hl_wdata;//ex hilo data
+    
     wire mem_rf_we;//ex段回写使能信号
     wire [4:0] mem_rf_waddr;//mem段回写寄存器
     wire [31:0] mem_rf_wdata;//mem段回写数据
+    wire mem_hl_we;//mem hilo we
+    wire [1:0]mem_hl_waddr;//mem hilo address
+    wire[63:0] mem_hl_wdata;//mem hilo data
     
     //reg线
-    wire [31:0] data1;
-    wire [31:0] data2;
     reg id_stop;
     always @ (posedge clk) begin
         if (rst) begin
@@ -56,17 +65,17 @@ module ID(
             if_to_id_bus_r <= if_to_id_bus;
             id_stop <=1'b0;
         end
-        else begin
+        else if (stall[1]==`Stop && stall[2]==`Stop) begin
             id_stop <=1'b1;
-            end
+        end
     end
     
     
-    assign ex_inst_isload =(ex_aluop==6'b10_0000)|
-                            (ex_aluop==6'b10_0100)|
-                            (ex_aluop==6'b10_0001)|
-                            (ex_aluop==6'b10_0101)|
-                            (ex_aluop==6'b10_0011);
+    assign ex_inst_isload = (ex_aluop==6'b10_0000)|
+                             (ex_aluop==6'b10_0100)|
+                             (ex_aluop==6'b10_0001)|
+                             (ex_aluop==6'b10_0101)|
+                             (ex_aluop==6'b10_0011);
     assign stallreq_id=(ex_inst_isload==1'b0)?1'b0:
                         (ex_addr==rs|ex_addr==rt)?1'b1:1'b0;
     assign inst=id_stop?inst:inst_sram_rdata;//PC对应的指令码
@@ -76,16 +85,25 @@ module ID(
     } = if_to_id_bus_r;
     //wb ex mem回写
     assign {
+        wb_hl_wdata,
+        wb_hl_waddr,
+        wb_hl_we,
         wb_rf_we,
         wb_rf_waddr,
         wb_rf_wdata
     } = wb_to_rf_bus;
     assign {
+        ex_hl_wdata,
+        ex_hl_waddr,
+        ex_hl_we,
         ex_rf_we,
         ex_rf_waddr,
         ex_rf_wdata
     }=ex_to_id_bus;
     assign {
+        mem_hl_wdata,
+        mem_hl_waddr,
+        mem_hl_we,
         mem_rf_we,
         mem_rf_waddr,
         mem_rf_wdata
@@ -115,27 +133,55 @@ module ID(
     
     wire rf_we;
     wire [4:0] rf_waddr;
+    wire hl_we;
+    wire [1:0] hl_waddr;
     wire sel_rf_res;
     wire [2:0] sel_rf_dst;
 
     wire [31:0] rdata1, rdata2;
-
+    
+    wire hilo_addr;
+    wire [31:0] p_rdata;
+    wire [31:0] data1;
+    wire [31:0] data2;
+    wire [63:0] p_data;
     regfile u_regfile(
-    	.clk    (clk    ),
-        .raddr1 (rs ),
-        .rdata1 (rdata1),
-        .raddr2 (rt ),
-        .rdata2 (rdata2 ),
+    	.clk    (clk          ),
+        .raddr1 (rs           ),
+        .rdata1 (rdata1       ),
+        .raddr2 (rt           ),
+        .rdata2 (rdata2       ),
         .we     (wb_rf_we     ),
         .waddr  (wb_rf_waddr  ),
-        .wdata  (wb_rf_wdata  )
+        .wdata  (wb_rf_wdata  ),
+        .p_raddr(hilo_addr    ),
+        .hilo_o (p_rdata      ),
+        .p_we   (wb_hl_we     ),
+        .p_waddr(wb_hl_waddr  ),
+        .p_wdata(wb_hl_wdata  )
     );
-    assign data1=(ex_rf_we==1'b1)&&(ex_rf_waddr==rs)?ex_rf_wdata:
-                (mem_rf_we==1'b1)&&(mem_rf_waddr==rs)?mem_rf_wdata:
-                (wb_rf_we==1'b1)&&(wb_rf_waddr==rs)?wb_rf_wdata:rdata1;
+    assign hilo_addr = inst_mflo ? 1'b1:1'b0;
+    //数据相关处理：id段需要读取hilo寄存器中的数据，因此需要ex mem wb回传最新的要存储在hilo寄存器中的值
+    assign p_data = (ex_hl_we==1'b1)&&(ex_hl_waddr[0]==1'b1)? ex_hl_wdata:
+                     (ex_hl_we==1'b1)&&(ex_hl_waddr[1]==1'b1)&&(ex_hl_waddr[1]==hilo_addr)?{ex_hl_wdata[63:32],32'b0}:
+                     (ex_hl_we==1'b1)&&(ex_hl_waddr[1]==1'b0)&&(ex_hl_waddr[1]==hilo_addr)?{32'b0,ex_hl_wdata[31:0]}:
+                     (mem_hl_we==1'b1)&&(mem_hl_waddr[0]==1'b1)? mem_hl_wdata:
+                     (mem_hl_we==1'b1)&&(mem_hl_waddr[1]==1'b1)&&(mem_hl_waddr[1]==hilo_addr)?{mem_hl_wdata[63:32],32'b0}:
+                     (mem_hl_we==1'b1)&&(mem_hl_waddr[1]==1'b0)&&(mem_hl_waddr[1]==hilo_addr)?{32'b0,mem_hl_wdata[31:0]}:
+                     (wb_hl_we==1'b1)&&(wb_hl_waddr[0]==1'b1)? wb_hl_wdata:
+                     (wb_hl_we==1'b1)&&(wb_hl_waddr[1]==1'b1)&&(wb_hl_waddr[1]==hilo_addr)?{wb_hl_wdata[63:32],32'b0}:
+                     (wb_hl_we==1'b1)&&(wb_hl_waddr[1]==1'b0)&&(wb_hl_waddr[1]==hilo_addr)?{32'b0,wb_hl_wdata[31:0]}:
+                     hilo_addr?{p_rdata,32'b0}:{32'b0,p_rdata};
+    
+    //数据相关处理
+    assign data1=(ex_rf_we==1'b1)&&(ex_rf_waddr==rs)? ex_rf_wdata:
+                 (mem_rf_we==1'b1)&&(mem_rf_waddr==rs)? mem_rf_wdata:
+                 (wb_rf_we==1'b1)&&(wb_rf_waddr==rs)? wb_rf_wdata:rdata1;
+                 
     assign data2=(ex_rf_we==1'b1)&&(ex_rf_waddr==rt)?ex_rf_wdata:
                  (mem_rf_we==1'b1)&&(mem_rf_waddr==rt)?mem_rf_wdata:
                  (wb_rf_we==1'b1)&&(wb_rf_waddr==rt)?wb_rf_wdata:rdata2;
+    
 
 
     assign opcode = inst[31:26];//取操作码
@@ -174,8 +220,10 @@ module ID(
     wire inst_j,inst_jal,inst_jr,inst_jalr;
     //分支指令
     wire inst_beq,inst_bne,inst_bgez,inst_bgtz,inst_blez,inst_bltz,inst_bgezal,inst_bltzal;
-    //空指令
-//    wire inst_nop;
+    //数据移动指令
+    wire inst_mfhi,inst_mflo,inst_mthi,inst_mtlo;
+    // 乘除指令
+    wire inst_mult,inst_multu,inst_div,inst_divu;
     decoder_6_64 u0_decoder_6_64(
     	.in  (opcode  ),
         .out (op_d )//运算的独热编码，只有一位为1表明该条指令的运算类型为这一种
@@ -195,10 +243,12 @@ module ID(
     	.in  (rt  ),
         .out (rt_d )//rt对应寄存器的独热编码，
     );
+    
     decoder_5_32 u2_decoder_5_32(
     	.in  (rd  ),
         .out (rd_d )//rt对应寄存器的独热编码，
     );
+    
     decoder_5_32 u3_decoder_5_32(
     	.in  (sa  ),
         .out (sa_d )//rt对应寄存器的独热编码，
@@ -254,37 +304,51 @@ module ID(
     assign inst_bltz    = op_d[6'b00_0001]&rt_d[5'b00000];//<0
     assign inst_bgezal  = op_d[6'b00_0001]&rt_d[5'b10001];//>=0
     assign inst_bltzal  = op_d[6'b00_0001]&rt_d[5'b10000];//<0
+    //数据移动指令
+    assign inst_mfhi    = op_d[6'b00_0000]&sa_d[5'b00000]&func_d[6'b01_0000];
+    assign inst_mflo    = op_d[6'b00_0000]&sa_d[5'b00000]&func_d[6'b01_0010];
+    assign inst_mthi    = op_d[6'b00_0000]&sa_d[5'b00000]&func_d[6'b01_0001];
+    assign inst_mtlo    = op_d[6'b00_0000]&sa_d[5'b00000]&func_d[6'b01_0011];
+    //乘除
+    assign inst_mult    = op_d[6'b00_0000]&sa_d[5'b00000]&func_d[6'b01_1000];
+    assign inst_multu   = op_d[6'b00_0000]&sa_d[5'b00000]&func_d[6'b01_1001];
+    assign inst_div     = op_d[6'b00_0000]&sa_d[5'b00000]&func_d[6'b01_1010];
+    assign inst_divu    = op_d[6'b00_0000]&sa_d[5'b00000]&func_d[6'b01_1011];
     
     // rs to reg1
     assign sel_alu_src1[0] = inst_ori|inst_addiu|inst_add|inst_addu|inst_addi|inst_sllv|inst_srav|inst_srlv
                              |inst_sub|inst_subu|inst_slt|inst_sltu|inst_slti|inst_sltiu
                              |inst_and|inst_andi|inst_nor|inst_or|inst_xor|inst_xori
-                             |inst_lw|inst_lb|inst_lbu|inst_lh|inst_lhu|inst_sb|inst_sh|inst_sw;//rs的值作src1
-
+                             |inst_lw|inst_lb|inst_lbu|inst_lh|inst_lhu|inst_sb|inst_sh|inst_sw
+                             |inst_mthi|inst_mtlo
+                             |inst_mult|inst_multu|inst_div|inst_divu;//rs的值作src1
+    
     // pc to reg1
     assign sel_alu_src1[1] = 1'b0|inst_bgezal|inst_bltzal|inst_jal|inst_jalr;//src1为pc值
-
+    
     // sa_zero_extend to reg1
     assign sel_alu_src1[2] = 1'b0|inst_sll|inst_srl|inst_sra;//无符号扩展数做src1
-
+    
     // rt to reg2
     assign sel_alu_src2[0] = 1'b0|inst_sub|inst_subu|inst_slt|inst_sltu|inst_and|inst_srav|inst_srlv
-                             |inst_nor|inst_or|inst_xor|inst_sll|inst_srl|inst_sra|inst_addu|inst_add|inst_sllv;//rt作src2
+                                  |inst_nor|inst_or|inst_xor|inst_sll|inst_srl|inst_sra|inst_addu|inst_add|inst_sllv
+                                  |inst_mult|inst_multu|inst_div|inst_divu;//rt作src2
     
     // imm_sign_extend to reg2
-    assign sel_alu_src2[1] = inst_lui | inst_addiu|inst_lw|inst_lb|inst_lbu|inst_lh|inst_lhu|inst_sb|inst_sh|inst_sw|inst_slti|inst_sltiu|inst_addi;//有符号扩展数作src2
+    assign sel_alu_src2[1] = 1'b0|inst_lui|inst_addiu|inst_lw|inst_lb|inst_lbu|inst_lh|inst_lhu|inst_sb|inst_sh|inst_sw|inst_slti|inst_sltiu|inst_addi;//有符号扩展数作src2
 
     // 32'b8 to reg2
     assign sel_alu_src2[2] = 1'b0|inst_bgezal|inst_bltzal|inst_jal|inst_jalr;//32位的整数8作为src2
 
     // imm_zero_extend to reg2
-    assign sel_alu_src2[3] = inst_ori|inst_andi|inst_xori;//立即数的无符号扩展作src2
+    assign sel_alu_src2[3] = 1'b0|inst_ori|inst_andi|inst_xori;//立即数的无符号扩展作src2
 
-
+    
 
     assign op_add = inst_addiu|inst_addu|inst_add|inst_addi
                               |inst_lw|inst_lb|inst_lbu|inst_lh|inst_lhu|inst_sb|inst_sh|inst_sw
-                              |inst_bgezal|inst_bltzal|inst_jal|inst_jalr;
+                              |inst_bgezal|inst_bltzal|inst_jal|inst_jalr
+                              |inst_mthi|inst_mtlo;
     assign op_sub = inst_sub |inst_subu;
     assign op_slt = inst_slt|inst_slti;
     assign op_sltu = inst_sltu|inst_sltiu;
@@ -306,26 +370,30 @@ module ID(
     // load and store enable
     assign data_ram_en = inst_lw|inst_lb|inst_lbu|inst_lh|inst_lhu|inst_sb|inst_sh|inst_sw;
 
-//    // write enable
+    // write enable
     assign data_ram_wen = inst_sb?4'b0001:
-                          inst_sh?4'b0011:
-                          inst_sw?4'b1111:
-                          inst_lb|inst_lbu|inst_lh|inst_lhu|inst_lw?4'b0000:4'b1110;
+                           inst_sh?4'b0011:
+                           inst_sw?4'b1111:
+                           inst_lb|inst_lbu|inst_lh|inst_lhu|inst_lw?4'b0000:4'b1110;
     //一个字节 还是 半字 还是字
     
     // regfile sotre enable
     assign rf_we = inst_ori |inst_lui|inst_sub|inst_subu|inst_srav|inst_srlv
-                            |inst_and|inst_andi|inst_nor|inst_or|inst_xor|inst_xori
-                            |inst_add|inst_addi|inst_addiu|inst_addu|inst_sllv
-                            |inst_slt|inst_slti|inst_sltiu|inst_sltu|inst_sll|inst_srl|inst_sra
-                            |inst_bgezal|inst_bltzal|inst_jal|inst_jalr
-                            |inst_lw|inst_lb|inst_lbu|inst_lh|inst_lhu;
+                             |inst_and|inst_andi|inst_nor|inst_or|inst_xor|inst_xori
+                             |inst_add|inst_addi|inst_addiu|inst_addu|inst_sllv
+                             |inst_slt|inst_slti|inst_sltiu|inst_sltu|inst_sll|inst_srl|inst_sra
+                             |inst_bgezal|inst_bltzal|inst_jal|inst_jalr
+                             |inst_lw|inst_lb|inst_lbu|inst_lh|inst_lhu
+                             |inst_mfhi|inst_mflo;
 
-
-
+    assign hl_we = inst_mthi|inst_mtlo
+                   |inst_mult|inst_multu|inst_div|inst_divu;//结果是不是在hilo寄存器上写
+    
+    
     // store in [rd]
     assign sel_rf_dst[0] = 1'b0|inst_sub|inst_subu|inst_addu|inst_and|inst_sllv|inst_srav|inst_srlv
-                               |inst_nor|inst_or|inst_xor|inst_slt|inst_sltu|inst_sll|inst_srl|inst_sra|inst_add|inst_jalr;
+                               |inst_nor|inst_or|inst_xor|inst_slt|inst_sltu|inst_sll|inst_srl|inst_sra|inst_add|inst_jalr
+                               |inst_mfhi|inst_mflo;
     // store in [rt] 
     assign sel_rf_dst[1] = inst_ori|inst_lui|inst_addiu|inst_lw|inst_lb|inst_lbu|inst_lh|inst_lhu|inst_slti|inst_sltiu|inst_sltiu|inst_addi|inst_andi|inst_xori;
     // store in [31]
@@ -335,11 +403,21 @@ module ID(
     assign rf_waddr = {5{sel_rf_dst[0]}} & rd 
                     | {5{sel_rf_dst[1]}} & rt
                     | {5{sel_rf_dst[2]}} & 32'd31;//选择结果存储的寄存器
-
+    
+    assign hl_waddr[0] = inst_mult|inst_multu|inst_div|inst_divu;//乘除法 hi和lo寄存器都要使用 数据移动指令仅使用hi 或 lo
+    assign hl_waddr[1] = inst_mtlo ? 1'b1:1'b0;//判断结果写在lo 还是 hi
+    
+    
     // 0 from alu_res ; 1 from ld_res
     assign sel_rf_res = 1'b0|inst_lw|inst_lb|inst_lbu|inst_lh|inst_lhu; //选择存储寄存器中的值的来源
  
+ 
+ 
+ 
     assign id_to_ex_bus = {
+        p_data,         //225:162
+        hl_waddr,        //161:160
+        hl_we,          //159
         id_pc,          // 158:127
         inst,           // 126:95
         alu_op,         // 94:83
@@ -389,6 +467,6 @@ module ID(
         br_e,
         br_addr
     };
-
-
+    
+    
 endmodule
